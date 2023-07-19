d1b48fd62d1775ee1729c98e4354841ac3387cbf
DESKTOP-DOGVPUB\SSAFY <smk921@naver.com>
2023-07-19 14:33:45 +0900

    📰Docs: BE 프로젝트 생성

---
+++ back_end/petmily_BE/src/main/java/com/example/petmily_BE/PetmilyBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.petmily_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmilyBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetmilyBeApplication.class, args);
+	}
+
+}
