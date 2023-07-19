bf5544c98445c7379c879df1bf33fbd0d0e0ea14
yms1789 <yms1789@naver.com>
2023-07-19 15:02:19 +0900

    🎉Chore: 펫밀리 프론트엔드 초기 설정 완료
    - 프로젝트 생성
    - ESLint 세팅
    - Prettier 세팅

---
+++ frontend/petmily/src/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'no-console': 0,
+  },
+};
