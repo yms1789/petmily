db2b7302d7cf32bcb309702047bd72e1bdd7eb44
kanghaeven <meerhae@gmail.com>
2023-07-21 12:31:29 +0900

    🚀Feat: 반려동물 정보 페이지 예외처리 구현 완료
    - 반려동물 이름, 성별, 소개에 값이 1개라도 없다면 버튼 비활성화
    - 이메일주소가 형식에 맞지 않거나 비밀번호가 양식에 맞지 않다면 에러 메시지 출력

--- frontend/petmily/src/pages/PetInfo.jsx
+++ frontend/petmily/src/pages/PetInfo.jsx
@@ -1,9 +1,10 @@
 import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
 import { styled } from '@mui/material';
-import { useState } from 'react';
-import axios from 'axios';
+import { useRef, useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
+import BACKEND_URL from '../utils/utils';
 
 function PetInfo() {
   const StyledArrowDropDownOutlinedIcon = styled(ArrowDropDownOutlinedIcon, {
@@ -14,74 +15,111 @@ function PetInfo() {
     fontSize: 40,
     '&:hover': { color: '#1f90fe' },
   });
-  // 초기값 세팅
   const [petname, setPetname] = useState('');
-  const [petgener, setPetgender] = useState(0);
+  const [petgender, setPetgender] = useState(0);
   const [petbirth, setPetbirth] = useState(new Date());
   const [petintro, setPetintro] = useState('');
-  // // 오류메세지 상태 저장
-  // const [petnameMessage, setPetnameMessage] = useState('');
-  // const [petgenerMessage, setPetgenderMessage] = useState('');
-  // const [petbirthMessage, setPetbirthMessage] = useState('');
-  // const [petintroMessage, setPetintroMessage] = useState('');
-  // // 유효성 검사
-  // const [isPetname, setIsPetname] = useState(false);
-  // const [isPetgender, setIsPetgender] = useState(false);
-  // const [isPetbirth, setIsPetbirth] = useState(false);
-  // const [isPetintro, setIsPetintro] = useState(false);
+  const [visiblePetnameError, setVisiblePetnameError] = useState(false);
+  const [visiblePetgenderError, setVisiblePetgenderError] = useState(false);
+  const [visiblePetintroError, setVisiblePetintroError] = useState(false);
+  const petnameInput = useRef(null);
+  const petgenderInput = useRef(null);
+  const petintroInput = useRef(null);
+  const checkForm = () => {
+    if (petname && petgender && petbirth && petintro) {
+      return true;
+    }
+    return false;
+  };
+
   const onChangePetname = e => {
-    const currentPetname = e.target.value;
-    setPetname(currentPetname);
-    // if (currentPetname.length < 1 || currentPetname.length > 8) {
-    //   setPetnameMessage(
-    //     '반려동물 이름은 1글자 이상 8글자 이하로 입력해주세요.',
-    //   );
-    //   setIsPetname(false);
-    // } else {
-    //   setPetnameMessage('멋진 이름이네요!');
-    //   setIsPetname(true);
-    // }
+    setVisiblePetnameError(false);
+    setPetname(e.target.value);
   };
   const onChangePetgender = e => {
-    const currentPetgender = e.target.value;
-    setPetgender(currentPetgender);
-    // if (currentPetgender === 0) {
-    //   setPetgenderMessage('반려동물 성별을 선택해주세요.');
-    //   setIsPetgender(false);
-    // } else {
-    //   setPetgenderMessage('머라 적징');
-    //   setIsPetgender(true);
-    // }
+    setVisiblePetgenderError(false);
+    setPetgender(e.target.value);
   };
   const onChangePetbirth = e => {
     setPetbirth(e); // Tue Jul 04 2023 16:57:01 GMT+0900 (한국 표준시)
   };
   const onChangePetintro = e => {
+    setVisiblePetintroError(false);
     setPetintro(e.target.value);
   };
-  const handleSubmit = e => {
+  const validatePetname = currentPetname => {
+    if (currentPetname.length < 1 || currentPetname.length > 8) {
+      return '반려동물 이름은 1글자 이상 8글자 이하로 입력해주세요.';
+    }
+    return '';
+  };
+  const validatePetgender = currentPetgender => {
+    if (currentPetgender === 0) {
+      return '반려동물 성별을 선택해주세요.';
+    }
+    return '';
+  };
+  const validatePetintro = currentPetintro => {
+    if (currentPetintro.length < 1) {
+      return '반려동물을 소개해주세요.';
+    }
+    return '';
+  };
+
+  const handlePetinfo = async (
+    currentPetname,
+    currentPetgender,
+    currentPetbirth,
+    currentPetintro,
+    e,
+  ) => {
+    // 백엔드에 반려동물 정보 전달
     e.preventDefault();
+    console.log(
+      'PetInfo',
+      currentPetname,
+      currentPetgender,
+      currentPetbirth,
+      currentPetintro,
+    );
     const petData = {
       petname,
-      petgener,
+      petgender,
       petbirth,
       petintro,
     };
-    axios
-      .post('http://192.168.100.208:3000/petinfo', petData)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        if (error.response) {
-          console.log(error.response);
-        } else if (error.request) {
-          console.log(error.request);
-        } else {
-          console.log(error);
-        }
-      });
+    try {
+      if (validatePetname(currentPetname)) {
+        throw new Error(validatePetname(currentPetname));
+      }
+      if (validatePetgender(currentPetgender)) {
+        throw new Error(validatePetgender(currentPetgender));
+      }
+      if (validatePetintro(currentPetintro)) {
+        throw new Error(validatePetintro(currentPetintro));
+      }
+      const response = await axios.post(BACKEND_URL, petData);
+      console.log(response);
+    } catch (error) {
+      console.log('error', error);
+      if (
+        error.message ===
+        '반려동물 이름은 1글자 이상 8글자 이하로 입력해주세요.'
+      ) {
+        setVisiblePetnameError(true);
+        currentPetname.current.focus();
+      }
+      if (error.message === '반려동물 성별을 선택해주세요.') {
+        setVisiblePetgenderError(true);
+        currentPetgender.current.focus();
+      }
+      if (error.message === '반려동물을 소개해주세요.') {
+        setVisiblePetintroError(true);
+        currentPetintro.current.focus();
+      }
+    }
   };
+
   return (
     <div className="relative bg-white w-full h-[111rem] overflow-y-auto text-left text-[1.25rem] text-gray font-pretendard">
       <div className="absolute top-[calc(50%_-_771px)] left-[calc(50%_-_332px)] rounded-[40px] bg-white w-[41.5rem] flex flex-col p-[2.5rem] box-border items-center justify-start gap-[3.63rem]">
@@ -115,14 +153,18 @@ function PetInfo() {
               className="flex-1 rounded-3xs box-border h-[3rem] flex flex-row px-[1rem] items-center justify-start border-[1.5px] border-solid border-darkgray"
               type="text"
               placeholder="반려동물 이름"
-              value={petname}
-              onChange={onChangePetname}
+              ref={petnameInput}
+              onChange={e => {
+                onChangePetname(e);
+              }}
             />
             <select
               name=""
               id=""
-              value={petgener}
-              onChange={onChangePetgender}
+              ref={petgenderInput}
+              onChange={e => {
+                onChangePetgender(e);
+              }}
               className="appearance-none flex-1 rounded-3xs box-border h-[3rem] px-[1rem] flex flex-row items-center justify-between border-[1.5px] border-solid border-darkgray"
             >
               <option value="0">성별</option>
@@ -133,6 +175,12 @@ function PetInfo() {
               <StyledArrowDropDownOutlinedIcon className="w-5 h-5 text-gray-400" />
             </div>
           </div>
+          {visiblePetnameError || validatePetname ? (
+            <span>반려동물 이름은 1글자 이상 8글자 이하로 입력해주세요.</span>
+          ) : null}
+          {visiblePetgenderError || validatePetgender ? (
+            <span>반려동물 성별을 선택해주세요.</span>
+          ) : null}
         </div>
         <div className="flex flex-col items-start justify-center">
           <div className="w-[36rem] flex flex-col items-start justify-start gap-[1rem]">
@@ -146,7 +194,9 @@ function PetInfo() {
               dateFormat="yyyy.MM.dd"
               shouldCloseOnSelect
               selected={petbirth}
-              onChange={onChangePetbirth}
+              onChange={e => {
+                onChangePetbirth(e);
+              }}
               className="w-[17.5rem] rounded-3xs box-border h-[3rem] px-[1rem] items-center justify-between border-[1.5px] border-solid border-darkgray"
             />
           </div>
@@ -164,16 +214,26 @@ function PetInfo() {
             cols="30"
             rows="10"
             placeholder="내용을 작성해주세요"
-            value={petintro}
-            onChange={onChangePetintro}
+            ref={petintroInput}
+            onChange={e => {
+              onChangePetintro(e);
+            }}
             className="rounded-3xs bg-white box-border w-[35.44rem] h-[21.69rem] flex flex-row py-[1.31rem] px-[1.56rem] items-start justify-start text-darkgray border-[1.5px] border-solid border-darkgray"
           />
+          {visiblePetintroError || validatePetintro ? (
+            <span>반려동물을 소개해주세요.</span>
+          ) : null}
         </div>
         <div className="relative w-[35.44rem] h-[4.5rem]">
           <button
             type="submit"
-            className="absolute top-[0rem] left-[0rem] rounded-[50px] bg-dodgerblue w-[35.44rem] h-[4.5rem]"
-            onClick={handleSubmit}
+            className={`${
+              checkForm() ? ' bg-dodgerblue' : 'bg-darkgray'
+            } absolute top-[0rem] left-[0rem] rounded-[50px] w-[35.44rem] h-[4.5rem]`}
+            onClick={e => {
+              handlePetinfo(petname, petgender, petbirth, petintro, e);
+            }}
+            disabled={!checkForm()}
           >
             <b className="absolute top-[1.31rem] left-[calc(50%_-_45.5px)] tracking-[0.01em] leading-[125%] text-[1.5rem] text-white">
               작성 완료
