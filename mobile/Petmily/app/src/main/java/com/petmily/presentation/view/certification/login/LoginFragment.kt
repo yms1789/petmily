79940191008b53c07d88e0a3b0ecd8d642714519
dyddn3994 <dyddn3994@naver.com>
2023-07-24 16:54:11 +0900

    🚀Feat: 로그인 통신코드 작성 완료
    - 이벤트 처리 부분 with(binding)으로 변경
    - 기본 통신 코드 작성 완료

--- mobile/Petmily/app/src/main/java/com/petmily/presentation/view/certification/login/LoginFragment.kt
+++ mobile/Petmily/app/src/main/java/com/petmily/presentation/view/certification/login/LoginFragment.kt
@@ -4,83 +4,90 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
 import com.petmily.R
 import com.petmily.config.BaseFragment
 import com.petmily.databinding.FragmentLoginBinding
 import com.petmily.presentation.view.certification.join.JoinFragment
 import com.petmily.presentation.view.certification.password.PasswordFragment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.petmily.presentation.viewmodel.UserViewModel
 
 private const val TAG = "Fetmily_LoginFragment"
-
 class LoginFragment :
     BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::bind, R.layout.fragment_login) {
+
+    private val userViewModel: UserViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initEditText()
         initBtn()
         initTextView()
+
+        initObserver()
     }
 
-    private fun initEditText() {
-        binding.apply {
-            // 이메일 입력
-            etId.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun afterTextChanged(p0: Editable?) {
-                    if (tilId.isErrorEnabled) tilId.isErrorEnabled = false
-                }
-            })
+    private fun initEditText() = with(binding) {
+        // 이메일 입력
+        etId.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                if (tilId.isErrorEnabled) tilId.isErrorEnabled = false
+            }
+        })
 
-            // 비밀번호 입력
-            etPwd.addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-                override fun afterTextChanged(p0: Editable?) {
-                    if (tilPwd.isErrorEnabled) tilPwd.isErrorEnabled = false
-                }
-            })
-        }
+        // 비밀번호 입력
+        etPwd.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {
+                if (tilPwd.isErrorEnabled) tilPwd.isErrorEnabled = false
+            }
+        })
     }
 
-    private fun initBtn() {
-        binding.apply {
-            // 로그인
-            btnLogin.setOnClickListener {
-                if (etId.text.isNullOrBlank()) tilId.error = getString(R.string.login_id_error)
-                if (etPwd.text.isNullOrBlank()) tilPwd.error = getString(R.string.login_pwd_error)
+    private fun initBtn() = with(binding) {
+        // 로그인
+        btnLogin.setOnClickListener {
+            if (etId.text.isNullOrBlank()) tilId.error = getString(R.string.login_id_error)
+            if (etPwd.text.isNullOrBlank()) tilPwd.error = getString(R.string.login_pwd_error)
 
-                if (tilId.error.isNullOrBlank() && tilPwd.error.isNullOrBlank()) {
-                    // TODO: 로그인 API 통신
-                    CoroutineScope(Dispatchers.IO).launch {
-//                        val user = RetrofitUtil.loginService.login(User(etId.text.toString(), etPwd.text.toString()))
-                    }
-                }
+            if (tilId.error.isNullOrBlank() && tilPwd.error.isNullOrBlank()) {
+                userViewModel.login(etId.text.toString(), etPwd.text.toString())
+                // 결과
             }
         }
     }
 
-    private fun initTextView() {
-        binding.apply {
-            // 비밀번호 재설정
-            tvPwdFind.setOnClickListener {
-                parentFragmentManager.commit {
-                    replace(R.id.fcv_main, PasswordFragment())
-                    addToBackStack("loginToPassword")
-                }
+    private fun initTextView() = with(binding) {
+        // 비밀번호 재설정
+        tvPwdFind.setOnClickListener {
+            parentFragmentManager.commit {
+                replace(R.id.fcv_main, PasswordFragment())
+                addToBackStack("loginToPassword")
             }
+        }
 
-            // 회원가입
-            tvSignup.setOnClickListener {
-                parentFragmentManager.commit {
-                    replace(R.id.fcv_main, JoinFragment())
-                    addToBackStack("loginToSignup")
-                }
+        // 회원가입
+        tvSignup.setOnClickListener {
+            parentFragmentManager.commit {
+                replace(R.id.fcv_main, JoinFragment())
+                addToBackStack("loginToSignup")
+            }
+        }
+    }
+
+    private fun initObserver() {
+        userViewModel.token.observe(viewLifecycleOwner) {
+            if (it.isNullOrBlank()) {
+                // 에러
+                showCustomToast("에러")
+            } else {
+                // 성공
+                showCustomToast("성공")
             }
         }
     }
---
+++ mobile/Petmily/app/src/main/java/com/petmily/repository/api/certification/login/LoginApi.kt
@@ -0,0 +1,10 @@
+package com.petmily.repository.api.certification.login
+
+import com.petmily.repository.dto.User
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface LoginApi {
+    @POST("/login")
+    suspend fun login(@Body body: User): String
+}
