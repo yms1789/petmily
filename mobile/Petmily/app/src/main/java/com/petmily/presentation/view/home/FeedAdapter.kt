e149478c34aa40f81298118c6281b9daebc79157
dyddn3994 <dyddn3994@naver.com>
2023-07-25 13:01:47 +0900

    💄Design: 홈 화면 피드 작성중

---
+++ mobile/Petmily/app/src/main/java/com/petmily/presentation/view/home/FeedAdapter.kt
@@ -0,0 +1,8 @@
+package com.petmily.presentation.view.home
+
+import androidx.recyclerview.widget.RecyclerView
+import com.petmily.databinding.ItemFeedBinding
+
+class FeedAdapter(): RecyclerView.Adapter<FeedAdapter.FeedViewHolder>() {
+    inner class FeedViewHolder(val binding: ItemFeedBinding):
+}
