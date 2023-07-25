9f5d0813ad4cb583ba4e50de089c7612c9efd81d
kanghaeven <meerhae@gmail.com>
2023-07-25 23:33:47 +0900

    🚑Fix: 소셜 로그인 네이버 커스텀 버튼 클릭
    - document.querySelector로 DOM 접근하여 버튼 클릭
    - 기존 버튼 hidden

--- frontend/petmily/src/components/LoginNaver.jsx
+++ frontend/petmily/src/components/LoginNaver.jsx
@@ -1,5 +1,5 @@
-// 네이버 로그인 JDK 사용로직
 import React, { useEffect } from 'react';
+import naverLoginButtonImage from '../static/images/naverLoginButton.png';
 
 const { naver } = window;
 
@@ -10,16 +10,39 @@ function LoginNaver() {
       callbackUrl: `${process.env.REACT_APP_NAVER_CALLBACK_URL}`,
       clientSecret: `${process.env.REACT_APP_NAVER_STATE_STRING}`,
       isPopup: true,
-      loginButton: { color: 'green', type: 1, height: '60' },
+      loginButton: {
+        color: 'green',
+        type: 1,
+        height: '60',
+      },
+      callbackHandle: 'true',
     });
     naverLogin.init();
+  };
+
+  const handleCustomNaverLogin = () => {
+    const naverLoginButton = document.querySelector('#naverIdLogin > a');
+    if (naverLoginButton) {
+      naverLoginButton.click();
+    }
   };
 
   useEffect(() => {
     initializeNaverLogin();
   }, []);
 
-  return <div id="naverIdLogin" />;
+  return (
+    <div>
+      <div role="presentation" onClick={handleCustomNaverLogin}>
+        <img
+          src={naverLoginButtonImage}
+          alt="네이버로그인버튼"
+          className="h-[4rem] w-[4rem] border-[1px] border-solid border-naver rounded-full"
+        />
+      </div>
+      <div id="naverIdLogin" className="hidden" />
+    </div>
+  );
 }
 
 export default LoginNaver;
