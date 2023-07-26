c42ca245133b52da99278fb43edcf71bc670425d
yms1789 <yms1789@naver.com>
2023-07-26 16:19:03 +0900

    🚀Feat: 더보기 버튼 핸들링 메서드 추가

--- frontend/petmily/src/components/RenderCuration.jsx
+++ frontend/petmily/src/components/RenderCuration.jsx
@@ -18,16 +18,23 @@ function RenderCuration({ animal }) {
     name: 'StyledArrowForwardIosRoundedIcon',
     slot: 'Wrapper',
   })({});
+
+  const handleShowMoreClick = () => {};
+
   return (
     <div className="min-w-[1340px] max-w-full flex flex-col items-start justify-start gap-[2.25rem]">
       <div className="self-stretch flex flex-row items-center justify-between">
-        <div className="rounded-31xl bg-dodgerblue overflow-hidden flex flex-row py-2.5 px-5 items-center justify-start gap-[0.63rem]">
+        <div className="rounded-31xl bg-dodgerblue overflow-hidden flex flex-row py-2.5 px-5 items-center justify-start gap-[0.5rem]">
           <StyledPetsIcon className="relative w-[14px] h-auto" />
-          <div className="relative tracking-[0.01em] leading-[125%] font-extrabold p-2 rounded-xl">
+          <div className="relative tracking-[0.01em] leading-[125%] font-extrabold p-2 rounded-xl text-xl">
             {animal}
           </div>
         </div>
-        <div className="rounded-31xl overflow-hidden flex flex-row py-2.5 px-5 items-center gap-[12px] text-[1.25rem] text-dodgerblue border-[2px] border-solid">
+        <div
+          className="rounded-31xl overflow-hidden flex flex-row py-2.5 px-5 items-center gap-[12px] text-[1.25rem] text-dodgerblue border-[2px] border-solid cursor-pointer"
+          role="presentation"
+          onClick={handleShowMoreClick}
+        >
           <b className="relative tracking-[0.01em] leading-[125%] text-dodgerblue">
             더보기
           </b>
