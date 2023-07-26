c96d9190037b817cd622b35ff5d4dcacb855beff
yms1789 <yms1789@naver.com>
2023-07-26 15:52:57 +0900

    🚀Feat: SearchBar 기능 및 컴포넌트 분할

--- frontend/petmily/src/components/SearchBar.jsx
+++ frontend/petmily/src/components/SearchBar.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import { string } from 'prop-types';
 
-function SearchBar() {
-  return (
-    <div className="border-solid border-2 border-dodgerblue mt-[5rem] top-[170px] rounded-11xl bg-white w-[760px] h-[60px] overflow-hidden">
-      <div className="relative top-[calc(50%_-_11px)] left-[39px] tracking-[0.01em] leading-[125%] font-medium">
-        검색어를 입력하세요
-      </div>
+function SearchBar({ page }) {
+  const [inputSearch, setInputSearch] = useState('');
+  return page !== '소통하기' ? (
+    <div className="flex flex-row justify-between items-center border-solid border-2 px-5 border-dodgerblue mt-[5rem] top-[170px] rounded-11xl bg-white w-[760px] h-[60px] overflow-hidden">
+      <input
+        className=" focus:outline-none w-full h-auto focus:border-dodgerblue focus:border-1.5 font-pretendard text-base
+        relative tracking-[0.01em] leading-[125%]"
+        placeholder="검색어를 입력하세요"
+        value={inputSearch}
+        onChange={e => setInputSearch(e.target.value)}
+      />
+      <SearchIcon
+        fontSize="large"
+        onClick={() => {
+          console.log('click');
+        }}
+      />
     </div>
-  );
+  ) : null;
 }
-
+SearchBar.propTypes = {
+  page: string,
+};
 export default SearchBar;
