30a869bc3280f7d1705581ba8a8ed1e1dc25947f
yms1789 <yms1789@naver.com>
2023-07-27 17:34:24 +0900

    💄Design: 큐레이션 렌더링 컴포넌트 CSS 수정
    - 인기 탭일 때 더보기 버튼 invisible, "인기" 버튼 디자인 수정

--- frontend/petmily/src/components/RenderCuration.jsx
+++ frontend/petmily/src/components/RenderCuration.jsx
@@ -6,12 +6,12 @@ import React from 'react';
 import { placeholderImage } from '../utils/utils';
 
 const placeholderCurations = Array(5).fill('');
-function RenderCuration({ animal }) {
+function RenderCuration({ category }) {
   const StyledPetsIcon = styled(PetsIcon, {
-    name: 'StyledArrowForwardIosRoundedIcon',
+    name: 'StyledPetsIcon',
     slot: 'Wrapper',
   })({
-    width: '40px',
+    width: '30px',
     height: 'auto',
   });
   const StyledArrowForwardIosRoundedIcon = styled(ArrowForwardIosRoundedIcon, {
@@ -24,22 +24,30 @@ function RenderCuration({ animal }) {
   return (
     <div className="min-w-[1340px] max-w-full flex flex-col items-start justify-start gap-[2.25rem] mb-5 mt-5">
       <div className="self-stretch flex flex-row items-center justify-between">
-        <div className="rounded-31xl bg-dodgerblue overflow-hidden flex flex-row py-2.5 px-5 items-center justify-start gap-[0.5rem]">
+        <div
+          className={`rounded-31xl ${
+            category === '인기'
+              ? ' bg-white text-dodgerblue border-solid border-2 border-dodgerblue'
+              : 'bg-dodgerblue'
+          } overflow-hidden flex flex-row py-2.5 px-5 items-center justify-start gap-[0.5rem]`}
+        >
           <StyledPetsIcon className="relative w-[14px] h-auto" />
-          <div className="relative tracking-[0.01em] leading-[125%] font-extrabold p-2 rounded-xl text-xl">
-            {animal}
+          <div className="relative tracking-[0.01em] leading-[125%] font-extrabold px-2 rounded-xl text-xl">
+            {category}
           </div>
         </div>
-        <div
-          className="rounded-31xl overflow-hidden flex flex-row py-2.5 px-5 items-center gap-[12px] text-[1.25rem] text-dodgerblue border-[2px] border-solid cursor-pointer"
-          role="presentation"
-          onClick={handleShowMoreClick}
-        >
-          <b className="relative tracking-[0.01em] leading-[125%] text-dodgerblue">
-            더보기
-          </b>
-          <StyledArrowForwardIosRoundedIcon className="text-dodgerblue" />
-        </div>
+        {category !== '인기' && (
+          <div
+            className="rounded-31xl overflow-hidden flex flex-row py-2.5 px-5 items-center bg-white gap-[12px] text-[1.25rem] text-dodgerblue border-[2px] border-solid cursor-pointer"
+            role="presentation"
+            onClick={handleShowMoreClick}
+          >
+            <b className="relative tracking-[0.01em] leading-[125%] text-dodgerblue">
+              더보기
+            </b>
+            <StyledArrowForwardIosRoundedIcon className="text-dodgerblue" />
+          </div>
+        )}
       </div>
       <div className="min-w-[1340px] max-w-full flex flex-row items-start justify-start gap-[24.96px] text-[1rem] text-gray">
         {placeholderCurations.map(() => {
@@ -49,12 +57,12 @@ function RenderCuration({ animal }) {
               <div className="flex flex-col items-start justify-center gap-[16px]">
                 <div className="flex flex-row items-center justify-center gap-[12px]">
                   <div className="relative tracking-[0.01em] leading-[125%] font-medium flex items-start">
-                    {animal} 유산균 급여 시 장점과 구매 전 체크리스트
+                    {category} 유산균 급여 시 장점과 구매 전 체크리스트
                     블라블라블라
                   </div>
                 </div>
                 <div className="relative text-[0.88rem] tracking-[0.01em] leading-[125%] text-darkgray flex items-center">
-                  {animal} 유산균 급여 시 장점과 구매 전 체크리스트는요
+                  {category} 유산균 급여 시 장점과 구매 전 체크리스트는요
                   블라블라블라...
                 </div>
               </div>
@@ -66,6 +74,6 @@ function RenderCuration({ animal }) {
   );
 }
 RenderCuration.propTypes = {
-  animal: string,
+  category: string,
 };
 export default RenderCuration;
