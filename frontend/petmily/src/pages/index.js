4c146f095ba70293a095b9656ed6e322ee7ec717
yms1789 <yms1789@naver.com>
2023-07-28 17:59:34 +0900

    📦️Chore:  index.js 코드 위치 수정

--- frontend/petmily/src/pages/index.js
+++ frontend/petmily/src/pages/index.js
@@ -1,9 +1,9 @@
 import Curation from './Curation';
 import Join from './Join';
+import Login from './Login';
+import PetInfo from './PetInfo';
 import Product from './Product';
 import Social from './Social';
 import UserInfo from './UserInfo';
-import PetInfo from './PetInfo';
-import Login from './Login';
 
-export { Curation, Product, Social, Join, UserInfo, PetInfo, Login };
+export { Curation, Join, Login, PetInfo, Product, Social, UserInfo };
