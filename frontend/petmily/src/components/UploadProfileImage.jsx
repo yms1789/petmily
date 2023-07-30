977650e5f81c2624476a98c2580cf20596b8a519
kanghaeven <meerhae@gmail.com>
2023-07-30 23:47:36 +0900

    🚑Fix: Userinfo 페이지 프로필 이미지 aws s3 업로드 후 프리뷰 가져오기 수정
    - react-aws-s3 삭제 후 aws-sdk 설치
    - 업로드된 이미지 url로 프리뷰 가져오기

--- frontend/petmily/src/components/UploadProfileImage.jsx
+++ frontend/petmily/src/components/UploadProfileImage.jsx
@@ -2,6 +2,7 @@ import AddToPhotosRoundedIcon from '@mui/icons-material/AddToPhotosRounded';
 import { styled } from '@mui/material';
 import { useRef } from 'react';
 import { string, func } from 'prop-types';
+import AWS from 'aws-sdk';
 
 function UploadProfileImage({ uploadedImage, setUploadedImage }) {
   const StyledAddToPhotosRoundedIcon = styled(AddToPhotosRoundedIcon, {
@@ -17,26 +18,55 @@ function UploadProfileImage({ uploadedImage, setUploadedImage }) {
   });
 
   const fileInputRef = useRef(null);
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  // AWS S3에 업로드하는 함수를 정의
+  const uploadFile = async file => {
+    // AWS S3 설정
+    AWS.config.update({
+      region: process.env.REACT_APP_BUCKET_REGION,
+      credentials: {
+        accessKeyId: process.env.REACT_APP_BUCKET_ACCESS,
+        secretAccessKey: process.env.REACT_APP_BUCKET_SECRET,
+      },
+    });
+
+    const s3 = new AWS.S3();
+
+    const uploadParams = {
+      Bucket: process.env.REACT_APP_BUCKET_NAME,
+      Key: file.name,
+      Body: file,
+      ACL: 'public-read', // 이미지를 public으로 업로드하여 URL 접근 가능하도록 설정
+    };
+
+    try {
+      const data = await s3.upload(uploadParams).promise();
+      console.log(data.Location); // 이미지 URL 확인
+      return data.Location; // 업로드된 이미지의 URL 반환
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  };
 
   const handleImageUpload = e => {
     const file = e.target.files[0];
     if (!file || !(file instanceof Blob)) {
-      console.error('올바른 파일을 선택해주세요.');
-      return null;
+      console.error('올바른 파일을 선택해주세요');
+      return;
     }
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    console.log(file);
-    return new Promise(resolve => {
-      reader.onload = () => {
-        setUploadedImage(reader.result || null);
-        resolve();
-      };
-    });
-  };
 
-  const handleImageClick = () => {
-    fileInputRef.current.click();
+    const reader = new FileReader();
+    reader.onload = async () => {
+      // 이미지 데이터가 업데이트된 이후에 업로드를 진행합니다.
+      const imageUrl = await uploadFile(file);
+      // 이미지 URL을 setUploadedImage에 전달합니다.
+      setUploadedImage(imageUrl);
+    };
+    reader.readAsDataURL(file);
   };
 
   return (
@@ -70,4 +100,5 @@ UploadProfileImage.propTypes = {
   uploadedImage: string,
   setUploadedImage: func,
 };
+
 export default UploadProfileImage;
