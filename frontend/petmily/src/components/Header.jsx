73844bb7f3ca3b5406b630c9f3bdfcd3d04c0231
여민수 <yms1789@naver.com>
2023-07-31 02:01:21 +0900

    🔥Remove: 불필요한 코드 삭제
    - Header 컴포넌트 내 임시 Select box UI 삭제

--- frontend/petmily/src/components/Header.jsx
+++ frontend/petmily/src/components/Header.jsx
@@ -1,20 +1,15 @@
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import { styled } from '@mui/material';
-import { bool } from 'prop-types';
 import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import headerLogo from '../static/images/headerLogo.svg';
-import { placeholderImage } from '../utils/utils';
-import CONSTANTS from '../utils/constants';
+import { useRecoilValue } from 'recoil';
+import authAtom from 'states/auth';
+import CONSTANTS from 'utils/constants';
+import { placeholderImage } from 'utils/utils';
+import headerLogo from 'static/images/headerLogo.svg';
+import CustomSelect from './CustomSelect';
 
-function Header({ isLoggedIn } = false) {
+function Header() {
   const [link, setLink] = useState('');
-  const StyledArrowDownIosRoundedIcon = styled(ArrowForwardIosRoundedIcon, {
-    name: 'StyledArrowDownIosRoundedIcon',
-    slot: 'Wrapper',
-  })({
-    transform: 'rotate(90deg)',
-  });
+  const auth = useRecoilValue(authAtom);
 
   return (
     <>
@@ -71,7 +66,7 @@ function Header({ isLoggedIn } = false) {
             {CONSTANTS.HEADER.SOCIAL}
           </Link>
         </div>
-        {isLoggedIn ? (
+        {!auth ? (
           <div className="flex items-center justify-between text-lg text-black relative">
             <div className="rounded-full flex items-center justify-center">
               <Link
@@ -100,16 +95,11 @@ function Header({ isLoggedIn } = false) {
                 alt=""
               />
             </div>
-            <div
-              className="rounded-31xl 
-                   bg-white text-black border-solid border-2 border-dodgerblue
-               overflow-hidden flex flex-row py-2.5 px-5 items-center justify-start gap-[0.5rem]"
-            >
-              <div className="relative tracking-[0.01em] leading-[125%] font-extrabold px-2 rounded-xl text-lg">
-                nickname
-              </div>
-              <StyledArrowDownIosRoundedIcon className="text-dodgerblue" />
-            </div>
+            <CustomSelect
+              component="header"
+              select="nickname"
+              options={['설정', '마이페이지', '로그아웃']}
+            />
           </div>
         )}
       </div>
@@ -117,7 +107,4 @@ function Header({ isLoggedIn } = false) {
     </>
   );
 }
-Header.propTypes = {
-  isLoggedIn: bool,
-};
 export default Header;
