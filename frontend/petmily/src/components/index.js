8f25643532d702ac0f7662ae4491bf06e673eee1
여민수 <yms1789@naver.com>
2023-07-31 01:37:27 +0900

    🔨Refactor: 상대경로 관리
    - components 내 파일 경로 관리를 위해 index.js에 파일들을 import 및 export

---
+++ frontend/petmily/src/components/index.js
@@ -0,0 +1,33 @@
+import CustomSelect from './CustomSelect';
+import FollowRecommend from './FollowRecommend';
+import Header from './Header';
+import GoogleLoginPage from './LoginGoogle';
+import KakaoLogin from './LoginKakao';
+import LoginKakaoCallback from './LoginKakaoCallback';
+import LoginNaver from './LoginNaver';
+import LoginNaverCallback from './LoginNaverCallback';
+import Messages from './Messages';
+import MyPetInfo from './MyPetInfo';
+import PasswordResetModal from './PasswordResetModal';
+import PortalPopup from './PortalPopup';
+import ProductCarousel from './ProductCarousel';
+import RenderCuration from './RenderCuration';
+import SearchBar from './SearchBar';
+
+export {
+  CustomSelect,
+  FollowRecommend,
+  Header,
+  GoogleLoginPage,
+  KakaoLogin,
+  LoginKakaoCallback,
+  LoginNaver,
+  LoginNaverCallback,
+  Messages,
+  MyPetInfo,
+  PasswordResetModal,
+  PortalPopup,
+  ProductCarousel,
+  RenderCuration,
+  SearchBar,
+};
