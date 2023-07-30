e9c7bcdf60fc0b5bf6ba12e7232c9412717c7746
여민수 <yms1789@naver.com>
2023-07-31 02:06:33 +0900

    🚀Feat: 추가된 페이지 경로 관리

--- frontend/petmily/src/pages/index.js
+++ frontend/petmily/src/pages/index.js
@@ -1,9 +1,23 @@
 import Curation from './Curation';
+import CurationCategory from './CurationCategory';
+import CurationPet from './CurationPet';
 import Join from './Join';
 import Login from './Login';
+import MyPage from './MyPage';
 import PetInfo from './PetInfo';
 import Product from './Product';
 import Social from './Social';
 import UserInfo from './UserInfo';
 
-export { Curation, Join, Login, PetInfo, Product, Social, UserInfo };
+export {
+  Curation,
+  CurationCategory,
+  CurationPet,
+  Join,
+  Login,
+  MyPage,
+  PetInfo,
+  Product,
+  Social,
+  UserInfo,
+};
