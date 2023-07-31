3371d866714761cbb3cb8ed55c7a1e56a57bce61
yms1789 <yms1789@naver.com>
2023-07-31 14:23:37 +0900

    🚑Fix: 새로고침 시에 select atom이 초기화되지 않도록 수정

--- frontend/petmily/src/states/select.js
+++ frontend/petmily/src/states/select.js
@@ -1,8 +1,11 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
+const { persistAtom } = recoilPersist();
 const selectAtom = atom({
   key: 'select',
   default: '',
+  effects_UNSTABLE: [persistAtom], // 새로고침 시에도 atom 데이터가 유지될 수 있도록하기 위함
 });
 
 export default selectAtom;
