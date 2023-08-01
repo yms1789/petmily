94beb39a8cbc9a4a588daee3f14abde94d021de2
yms1789 <yms1789@naver.com>
2023-08-01 17:45:13 +0900

    🚀Feat: 최저가 탭 캐러셀 디자인 및 기능 수정

--- frontend/petmily/src/components/ProductCarousel.jsx
+++ frontend/petmily/src/components/ProductCarousel.jsx
@@ -1,32 +1,37 @@
 import { Paper } from '@mui/material';
 import Carousel from 'react-material-ui-carousel';
-import { placeholderImage } from 'utils/utils';
 
 const placeholderData = Array.from(
   { length: 5 },
-  (_, i) => `https://picsum.photos/1920/1920/?image=${i + 1}`,
+  (_, i) => `https://picsum.photos/1920/1000/?image=${i + 1}`,
 );
 function ProductCarousel() {
   return (
-    <div className="relative flex flex-col mt-20 text-[2.38rem] text-white min-w-[1320px] w-full">
-      <div className="gap-2 flex flex-row min-w-[1320px] max-w-full">
-        <Carousel className="w-full" autoPlay={false} indicators={false}>
-          {placeholderData.map(ele => {
-            console.log(ele);
-            return (
-              <Paper key={ele}>
-                <div className="flex-1 flex w-full self-stretch">
-                  <img
-                    className="relative w-full h-[30rem]"
-                    alt={placeholderImage}
-                    src={ele}
-                  />
-                </div>
-              </Paper>
-            );
-          })}
-        </Carousel>
-      </div>
+    <div
+      role="presentation"
+      className="relative flex flex-col mt-10 text-[2.38rem] text-white min-w-[1320px] w-full"
+    >
+      <Carousel
+        className="w-full"
+        indicators={false}
+        autoPlay
+        fullHeightHover
+        swipe
+      >
+        {placeholderData.map(ele => {
+          return (
+            <Paper key={ele}>
+              <div className="flex-1 flex w-full">
+                <img
+                  className="relative w-full h-[30rem] object-fit"
+                  alt=""
+                  src={ele}
+                />
+              </div>
+            </Paper>
+          );
+        })}
+      </Carousel>
       <div className="absolute top-[25rem] left-0 brightness-125 flex flex-1 flex-col z-[1]">
         <div
           className="relative flex flex-row justify-center items-center bg-dodgerblue [backdrop-filter:blur(100px)]
