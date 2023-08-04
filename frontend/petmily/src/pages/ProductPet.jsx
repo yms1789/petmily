245f847d488639fa10b1c2713c5f3ae173e5e63a
yms1789 <yms1789@naver.com>
2023-08-04 17:48:30 +0900

    🚀Feat: 최저가 상품 기능 추가
    - 서버에서 데이터 가져와서 렌더링 할 수 있도록 기능 추가

--- frontend/petmily/src/pages/ProductPet.jsx
+++ frontend/petmily/src/pages/ProductPet.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { ProductCarousel, RenderProducts, SearchBar } from 'components';
 import selectAtom from 'states/select';
 import CustomSelect from 'components/CustomSelect';
 import searchAtom from 'states/search';
+import useFetch from 'utils/fetch';
+import productAtom from 'states/products';
 
-const productCategories = ['식품', '미용', '건강', '기타'];
+const productCategories = ['식품', '미용', '건강'];
 function ProductPet() {
   const select = useRecoilValue(selectAtom);
   const altSelect = decodeURIComponent(
     window.location.pathname.split('/').at(-1),
   );
   const searchResult = useRecoilValue(searchAtom);
+  const [globalProduct, setGlobalProduct] = useRecoilState(productAtom);
   const [isSearch, setIsSearch] = useState(false);
+  const fetchData = useFetch();
+  useEffect(() => {
+    const fetchPetData = async () => {
+      try {
+        const petSelect = decodeURIComponent(
+          window.location.pathname.split('/').at(-1),
+        );
+        const curationData = await fetchData.get(
+          `/product/getdata?species=${petSelect}`,
+        );
+        console.log('fetchData', curationData['식품']);
+
+        setGlobalProduct({
+          식품: curationData['식품'],
+          건강: curationData['건강'],
+          미용: curationData['미용'],
+          기타: curationData['기타'],
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchPetData();
+    return () => {};
+  }, []);
+
   return (
     <div className="bg-whitesmoke  min-w-[1340px] max-w-full flex flex-1 flex-col items-center justify-center text-left text-[1.13rem] text-darkgray font-pretendard">
       <div className="min-w-[1340px] max-w-full w-full relative text-[1.75rem] text-gray">
@@ -36,12 +65,16 @@ function ProductPet() {
                 petCategory={select.length > 0 ? select : altSelect}
                 setIsSearch={setIsSearch}
               />
-            </div>
+            </div>{' '}
             {isSearch ? (
               <RenderProducts category="검색" renderData={searchResult} />
             ) : (
               productCategories.map(category => (
-                <RenderProducts category={category} showMore />
+                <RenderProducts
+                  category={category}
+                  showMore
+                  renderData={globalProduct[category]}
+                />
               ))
             )}
           </div>
