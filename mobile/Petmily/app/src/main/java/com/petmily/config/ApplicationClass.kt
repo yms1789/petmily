2a7a42ffe9553642994703e2861956a94901fc9c
dongHyeok <mimiga1@naver.com>
2023-08-04 17:56:51 +0900

    🚀Feat: 8083 => 8088 포트 변경

--- mobile/Petmily/app/src/main/java/com/petmily/config/ApplicationClass.kt
+++ mobile/Petmily/app/src/main/java/com/petmily/config/ApplicationClass.kt
@@ -14,7 +14,7 @@ import java.util.concurrent.TimeUnit
 // 앱이 실행될때 1번만 실행이 됩니다.
 class ApplicationClass : Application() {
     // ends with '/'
-    val API_URL = "http://3.34.187.150:8083/"
+    val API_URL = "http://3.34.187.150:8088/"
 
     // 코틀린의 전역변수
     companion object {
