702b7ed44b549dd42ba9d9350ef5c5f4e2f7c826
여민수 <yms1789@naver.com>
2023-08-04 09:04:55 +0900

    🚀Feat: 아이콘 추가한 부분 렌더링, 기능 추가
    - 이미지 클릭 시 해당 동물 최저가 탭으로 이동할 수 있도록 기능 추가

--- frontend/petmily/src/pages/ProductPet.jsx
+++ frontend/petmily/src/pages/ProductPet.jsx
@@ -9,29 +9,31 @@ import searchAtom from 'states/search';
 const productCategories = ['식품', '미용', '건강', '기타'];
 function ProductPet() {
   const select = useRecoilValue(selectAtom);
+  const altSelect = decodeURIComponent(
+    window.location.pathname.split('/').at(-1),
+  );
   const searchResult = useRecoilValue(searchAtom);
   const [isSearch, setIsSearch] = useState(false);
   return (
     <div className="bg-whitesmoke  min-w-[1340px] max-w-full flex flex-1 flex-col items-center justify-center text-left text-[1.13rem] text-darkgray font-pretendard">
-      <div className="min-w-[1340px] max-w-full relative text-[1.75rem] text-gray">
+      <div className="min-w-[1340px] max-w-full w-full relative text-[1.75rem] text-gray">
         <div className=" flex flex-col items-start justify-start text-[1.5rem] text-white">
           <ProductCarousel />
           <div className="h-10" />
-          <b className="text-center self-stretch relative text-13xl tracking-[0.01em] leading-[125%] text-black">
-            HOT ITEM
-          </b>
+
           <div className="h-10" />
           <div className="p-8 w-[96%] min-w-[1340px] max-w-full">
             <div className="flex flex-row justify-around items-center w-full h-auto mt-20 mb-20">
               <div className="relative flex gap-5 justify-start flex-row w-full h-auto">
                 <CustomSelect
-                  select={select}
+                  component="product"
+                  select={select.length > 0 ? select : altSelect}
                   options={['강아지', '고양이', '기타동물']}
                 />
               </div>
               <SearchBar
                 page="최저가"
-                petCategory={select}
+                petCategory={select.length > 0 ? select : altSelect}
                 setIsSearch={setIsSearch}
               />
             </div>
