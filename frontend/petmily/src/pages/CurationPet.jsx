6d997435c4268033f0762ae03d0f81955a8e4a0e
yms1789 <yms1789@naver.com>
2023-08-07 13:25:30 +0900

    🚑Fix: 더보기 클릭했을 때 데이터가 null인 경우 에러나는 현상 수정
    - 더보기 눌렀을 때 서버에서 데이터를 요청해서 저장해야함
      - CurationPet 컴포넌트에 있던 setGlobalProduct 부분을 renderCuration으로 이동

--- frontend/petmily/src/pages/CurationPet.jsx
+++ frontend/petmily/src/pages/CurationPet.jsx
@@ -1,46 +1,32 @@
 import { Suspense, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { RenderCuration } from 'components';
 import CustomSelect from 'components/CustomSelect';
 import selectAtom from 'states/select';
 import { curationsAtom } from 'states/curations';
-import useFetch from 'utils/fetch';
 // import { placeholderImage } from 'utils/utils';
 import { ReactComponent as DogBanner } from 'static/images/dogBanner.svg';
+
+const category = ['건강', '미용', '식품', '입양'];
 
 function CurationPet() {
   const select = useRecoilValue(selectAtom);
   const location = useLocation();
-  const fetchData = useFetch();
   const { petType } = location.state;
-  console.log(petType);
   const [curations, setCurations] = useState([]);
-  const [globalCuration, setGlobalCurations] = useRecoilState(curationsAtom);
+  const globalCuration = useRecoilValue(curationsAtom);
   useEffect(() => {
-    const fetchPetData = async () => {
-      try {
-        const curationData = await fetchData.get(
-          `curation/getNewsData?species=${petType}`,
-        );
-        console.log('fetchData', curationData[petType]);
-        setCurations(
-          curationData[petType].filter(ele => ele.ccategory === '건강'),
-        );
-        setGlobalCurations({
-          건강: curationData[petType].filter(ele => ele.ccategory === '건강'),
-          식품: curationData[petType].filter(ele => ele.ccategory === '식품'),
-          미용: curationData[petType].filter(ele => ele.ccategory === '미용'),
-          입양: curationData[petType].filter(ele => ele.ccategory === '입양'),
-        });
-      } catch (error) {
-        console.log(error);
-      }
+    const fetchPetData = () => {
+      const filterFirstElement = [];
+      category.forEach(ele => {
+        filterFirstElement.push(globalCuration[ele][0]);
+      });
+      setCurations(filterFirstElement);
     };
     fetchPetData();
-    return () => {};
-  }, []);
+  }, [globalCuration, petType]);
 
   return (
     <div className="bg-whitesmoke  min-w-[1340px] max-w-full flex flex-1 flex-col items-center justify-center text-left text-[1.13rem] text-darkgray font-pretendard">
@@ -55,10 +41,7 @@ function CurationPet() {
             HOT TOPIC
           </b>
           <Suspense fallback={<p>글목록 로딩중...</p>}>
-            <RenderCuration
-              category="인기"
-              renderData={curations.slice(10, 13)}
-            />
+            <RenderCuration category="인기" renderData={curations} />
           </Suspense>
           <div className="h-10" />
           <div className="flex flex-row justify-around items-center w-full h-auto mt-20 mb-20">
