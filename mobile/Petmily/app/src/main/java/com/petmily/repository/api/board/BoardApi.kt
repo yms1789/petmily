a5ea3a0c055a33374e8271f411641fd5fe8fdb58
dyddn3994 <dyddn3994@naver.com>
2023-08-07 13:48:54 +0900

    🚑Fix: 홈 피드 api 변경에 따른변경 및 프로필 cardview 수정

--- mobile/Petmily/app/src/main/java/com/petmily/repository/api/board/BoardApi.kt
+++ mobile/Petmily/app/src/main/java/com/petmily/repository/api/board/BoardApi.kt
@@ -10,6 +10,7 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Part
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BoardApi {
     @Multipart
@@ -35,7 +36,7 @@ interface BoardApi {
     
     @GET("/board/all")
     suspend fun boardSelectAll(
-        @Path("currentUserEmail") currentUserEmail: String,
+        @Query("currentUserEmail") currentUserEmail: String,
     ): List<Board>
     
     @GET("/board/{boardId}")
