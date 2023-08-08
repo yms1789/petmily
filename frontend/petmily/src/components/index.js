d76e563d078b15c02acc36ba6f89a6f02cd20600
yms1789 <yms1789@naver.com>
2023-08-08 17:57:27 +0900

    📰New: 포인트 로그, 가챠 컴포넌트 추가

--- frontend/petmily/src/components/index.js
+++ frontend/petmily/src/components/index.js
@@ -1,6 +1,8 @@
 import DeleteConfirmation from './DeleteConfirmation';
 import FollowRecommend from './FollowRecommend';
+import GatchaComponent from './GatchaComponent';
 import Header from './Header';
+import Inventory from './Inventory';
 import LoginGoogle from './LoginGoogle';
 import LoginKakao from './LoginKakao';
 import LoginKakaoCallback from './LoginKakaoCallback';
@@ -9,8 +11,10 @@ import LoginNaverCallback from './LoginNaverCallback';
 import Messages from './Messages';
 import MyPetInfo from './MyPetInfo';
 import PasswordResetModal from './PasswordResetModal';
+import PointLog from './PointLog';
 import PortalPopup from './PortalPopup';
 import ProductCarousel from './ProductCarousel';
+import RenderBookmarks from './RenderBookmarks';
 import RenderCuration from './RenderCuration';
 import RenderProducts from './RenderProducts';
 import SearchBar from './SearchBar';
@@ -19,15 +23,13 @@ import SocialCommentInput from './SocialCommentInput';
 import SocialPost from './SocialPost';
 import SocialRecomment from './SocialRecomment';
 import UploadImage from './UploadImage';
-import GatchaComponent from './GatchaComponent';
-import PointLog from './PointLog';
 
 export {
-  PointLog,
-  GatchaComponent,
   DeleteConfirmation,
   FollowRecommend,
+  GatchaComponent,
   Header,
+  Inventory,
   LoginGoogle,
   LoginKakao,
   LoginKakaoCallback,
@@ -36,8 +38,10 @@ export {
   Messages,
   MyPetInfo,
   PasswordResetModal,
+  PointLog,
   PortalPopup,
   ProductCarousel,
+  RenderBookmarks,
   RenderCuration,
   RenderProducts,
   SearchBar,
