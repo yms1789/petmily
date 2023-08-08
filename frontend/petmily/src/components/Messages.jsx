1ca779446b4e641b8c1dac4d404b90df1197833a
kanghaeven <meerhae@gmail.com>
2023-08-08 17:53:51 +0900

    🚀Feat:: chat 페이지 라우팅 추가
    - Social feed
    - Social chat

--- frontend/petmily/src/components/Messages.jsx
+++ frontend/petmily/src/components/Messages.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import { placeholderImage } from 'utils/utils';
 
 const tempMessages = Array.from({ length: 5 }, (_, i) => i);
 
 function Messages() {
+  const navigate = useNavigate();
+
+  const handleOpenChat = () => {
+    navigate('/social/chat');
+  };
+
   return (
     <div className="mx-4 basis-1/4 flex h-[840px] rounded-xl bg-white min-w-[20%] flex-col p-[1rem] items-start justify-start gap-[0.38rem] font-pretendard">
       <div className="flex w-full flex-col items-start justify-center gap-[1rem] text-[1.25rem]">
@@ -12,8 +19,10 @@ function Messages() {
       {tempMessages.map(ele => {
         return (
           <div
+            role="presentation"
             key={ele}
             className="self-stretch flex flex-col items-start justify-start gap-[0.63rem]"
+            onClick={handleOpenChat}
           >
             <div className="w-full flex flex-row py-[0.75rem] px-[1rem] box-border items-center justify-between">
               <div className="w-fill gap-4 flex flex-row items-center justify-between">
@@ -31,7 +40,7 @@ function Messages() {
                   </div>
                 </div>
               </div>
-              <div className="rounded-full bg-dodgerblue h-8 w-8 overflow-hidden whitespace-nowrap flex flex-row box-border items-center justify-center text-center text-[1rem] text-white">
+              <div className="rounded-full bg-dodgerblue h-8 w-8 overflow-hidden whitespace-nowrap flex flex-row box-border items-center justify-center text-center text-sm text-white">
                 <b className="">{ele}</b>
               </div>
             </div>
