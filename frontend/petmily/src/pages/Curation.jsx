f4eb7fb53d5b4a0b75ef59913611b33eebd2186e
여민수 <yms1789@naver.com>
2023-08-09 00:29:42 +0900

    💄Design: 큐레이션 메인페이지 배너 사진 삭제

--- frontend/petmily/src/pages/Curation.jsx
+++ frontend/petmily/src/pages/Curation.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { CircularProgress } from '@mui/material';
 import useFetch from 'utils/fetch';
-import { placeholderImage } from 'utils/utils';
 import { RenderCuration } from 'components';
 
 function Curation() {
@@ -30,12 +29,7 @@ function Curation() {
     <div className="bg-whitesmoke  min-w-[1340px] max-w-full flex flex-1 flex-col items-center justify-center text-left text-[1.13rem] text-darkgray font-pretendard">
       <div className="min-w-[1340px] w-[95%] py-2 px-10 relative text-[1.75rem] text-gray">
         <div className="flex flex-col items-start justify-start text-[1.5rem] text-white">
-          <img
-            className="relative w-full h-[200px] rounded-[20px]"
-            alt=""
-            src={placeholderImage(Math.floor(Math.random() * 1001))}
-          />
-          <div className="h-20" />
+          <div className="h-48" />
           <b className="text-center self-stretch relative text-13xl tracking-[0.01em] leading-[125%] text-black">
             HOT TOPIC
           </b>
