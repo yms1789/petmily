3dba354d43341e63b1a24c7fdad491257972ae16
kanghaeven <meerhae@gmail.com>
2023-08-08 17:51:46 +0900

    🚀Feat: 소통하기 팔로우 추천 버튼 클릭 UI 구현

--- frontend/petmily/src/components/FollowRecommend.jsx
+++ frontend/petmily/src/components/FollowRecommend.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import { placeholderImage } from 'utils/utils';
 
 const tempMessages = Array.from({ length: 5 }, (_, i) => i);
 function FollowRecommend() {
+  const [showOnFollow, setShowOnFollow] = useState(false);
+
+  const handleFollow = () => {
+    setShowOnFollow(!showOnFollow);
+  };
+
   return (
     <div className="mx-4 basis-1/4 flex h-[840px] rounded-xl bg-white min-w-[20%] flex-col p-[1rem] items-start justify-start gap-[0.38rem] font-pretendard">
       <div className="flex w-full flex-col items-start justify-center gap-[1rem] text-[1.25rem]">
@@ -30,8 +37,16 @@ function FollowRecommend() {
                   </div>
                 </div>
               </div>
-              <div className="rounded-full bg-dodgerblue h-8 w-fill px-3 overflow-hidden whitespace-nowrap flex flex-row box-border items-center justify-center text-center text-[1rem] text-white">
-                <p className="">팔로우</p>
+              <div
+                role="presentation"
+                className={`${
+                  showOnFollow
+                    ? 'bg-white font-bold'
+                    : 'bg-dodgerblue text-white'
+                } text-dodgerblue border-dodgerblue border-[2px] border-solid  rounded-full h-7 w-fill px-2.5 overflow-hidden whitespace-nowrap flex flex-row box-border items-center justify-center text-center`}
+                onClick={handleFollow}
+              >
+                <p className="text-sm">팔로우</p>
               </div>
             </div>
             {ele < tempMessages.length - 1 ? (
