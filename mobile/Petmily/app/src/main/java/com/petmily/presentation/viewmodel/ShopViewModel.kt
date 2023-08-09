e361fcbc71bc5d58aa7e1697c714419c5a299d8f
dongHyeok <mimiga1@naver.com>
2023-08-09 16:25:31 +0900

    🚀Feat: store로직 완성(API연결 필요)

--- mobile/Petmily/app/src/main/java/com/petmily/presentation/viewmodel/ShopViewModel.kt
+++ mobile/Petmily/app/src/main/java/com/petmily/presentation/viewmodel/ShopViewModel.kt
@@ -1,5 +1,22 @@
 package com.petmily.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ShopViewModel : ViewModel()
+class ShopViewModel : ViewModel() {
+    
+    // 아이템 뽑기 결과 수신  -> 수정 필요(반환값 자료형을 아이템 객체로 변환 필요)
+    private var _resultItem = MutableLiveData<Boolean>()
+    val resultItem: LiveData<Boolean>
+        get() = _resultItem
+    
+    // 아이템 뽑기 요청
+    
+    
+    fun setResultItem(result: Boolean) {
+        _resultItem.value = result
+    }
+    
+    
+}
