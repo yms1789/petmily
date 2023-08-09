9b85fba71a40e7dcaafcd3d2ef946781eec5fbe5
yms1789 <yms1789@naver.com>
2023-08-09 15:08:47 +0900

    🚀Feat: 가챠 시나리오에 따른 기능 추가
    - 서버에 가챠 종류에 따른 결과 요청
    - 가챠 결과를 모달에 렌더링

--- frontend/petmily/src/components/GachaLoadingModal.jsx
+++ frontend/petmily/src/components/GachaLoadingModal.jsx
@@ -1,24 +1,37 @@
-import { func, string } from 'prop-types';
 import { useEffect } from 'react';
-import { Player } from '@lottiefiles/react-lottie-player';
+import { func, string } from 'prop-types';
+import axios from 'axios';
+import { useRecoilValue } from 'recoil';
 
-import gacha from 'static/animations/gacha.json';
+import { Player } from '@lottiefiles/react-lottie-player';
+import gachaLoading from 'static/animations/gachaLoading.json';
+import userAtom from 'states/users';
 
 function GachaLoadingModal({ onClose, gachaOpen, gachaSelect, setGachaItem }) {
+  const user = useRecoilValue(userAtom);
   useEffect(() => {
     // 서버에 뽑기 요청
     console.log('뽑기 요청');
-    setTimeout(() => {
-      onClose();
-      console.log(gachaSelect);
-      setGachaItem('뱃지12');
-      gachaOpen();
-    }, 4000);
-  }, []);
+    async function fetchData() {
+      try {
+        const response = await axios.post('item/getRandom', {
+          userEmail: user.userEmail,
+          randomKind: gachaSelect,
+        });
+        onClose();
+        console.log(response);
+        setGachaItem(response.data);
+        gachaOpen();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [gachaOpen, gachaSelect, onClose, setGachaItem, user.userEmail]);
 
   return (
     <div className="relative rounded-[10px] bg-inherit w-[656px] h-[450px] max-w-full flex flex-col justify-center items-center max-h-full text-left text-xl text-darkgray font-pretendard">
-      <Player loop src={gacha} autoplay />
+      <Player loop src={gachaLoading} autoplay />
     </div>
   );
 }
--- frontend/petmily/src/components/GachaModal.jsx
+++ frontend/petmily/src/components/GachaModal.jsx
@@ -1,13 +1,28 @@
-// import { Player } from '@lottiefiles/react-lottie-player';
+import { Player } from '@lottiefiles/react-lottie-player';
 
 import { string } from 'prop-types';
 
 // import gacha from 'static/animations/gacha.json';
+import gachaBoom from 'static/animations/gachaBoom.json';
 
 function GachaModal({ gachaItem }) {
+  console.log(gachaItem);
   return (
-    <div className="relative rounded-[10px] bg-inherit w-[656px] h-[450px] max-w-full flex flex-col justify-center items-center bg-white max-h-full text-left text-xl text-darkgray font-pretendard">
-      {gachaItem} 가챠 뽑았다!
+    <div
+      className={`relative rounded-[10px] bg-inherit w-[656px] h-[450px] max-w-full flex flex-col justify-center items-center ${
+        gachaItem ? 'bg-white' : 'bg-inherit'
+      } max-h-full text-left text-xl text-darkgray font-pretendard`}
+    >
+      {gachaItem ? (
+        <div>{gachaItem}</div>
+      ) : (
+        <>
+          <Player loop src={gachaBoom} autoplay />
+          <h1 className="absolute top-[-20px] left-24 text-[100px] text-white">
+            나가!
+          </h1>
+        </>
+      )}
     </div>
   );
 }
@@ -15,5 +30,4 @@ function GachaModal({ gachaItem }) {
 GachaModal.propTypes = {
   gachaItem: string,
 };
-
 export default GachaModal;
