22fbca6834011f77e98b915ee0a25a18b0c0b1c4
yms1789 <yms1789@naver.com>
2023-08-11 16:47:25 +0900

    🚀Feat: 상점 기능 추가 구현
    - 포인트 사용 내역을 서버에서 받아올 수 있도록 네트워크 통신 기능 추가
    - 인벤토리 정보를 서버에서 받아올 수 있도록 네트워크 통신 기능 추가

--- frontend/petmily/src/components/PointLog.jsx
+++ frontend/petmily/src/components/PointLog.jsx
@@ -1,45 +1,54 @@
 import { shape, number, string, instanceOf, arrayOf } from 'prop-types';
+import { priceToString } from 'utils/utils';
 
 function PointLog({ logs }) {
   return (
-    <div className="rounded-11xl bg-white min-w-[350px] h-fit overflow-hidden shrink-0 flex flex-col basis-1/4 p-4 box-border items-start justify-start gap-[6px]">
+    <div className="rounded-11xl bg-white min-w-[350px] h-fit overflow-hidden shrink-0 flex flex-col basis-1/4 p-4 box-border items-center justify-start gap-[6px]">
       <div className="flex flex-col items-start justify-center gap-[17px] w-full">
         <div className="relative font-semibold">포인트 내역</div>
         <div className="relative bg-whitesmoke-200 w-full h-px" />
       </div>
-      {logs.map((log, idx) => {
-        return (
-          <div className="self-stretch flex flex-col items-start justify-start gap-[6px]">
-            <div className="self-stretch rounded-11xl bg-white flex flex-row p-3 items-start justify-start gap-[24px]">
-              <img
-                className="self-stretch relative max-h-full w-[3px]"
-                alt=""
-                src="/curation-title-line.svg"
-              />
-              <div className="w-[319px] flex flex-col items-start justify-start gap-[12px]">
-                <div className="self-stretch flex flex-row items-center justify-start gap-[6px]">
-                  <div
-                    className={`relative tracking-[0.05em] leading-[125%] font-semibold ${
-                      log.type ? 'text-dodgerblue' : 'text-red'
-                    }`}
-                  >
-                    {log.type ? `+${log.point}` : `-${log.point}`}
+      {logs ? (
+        logs.map((log, idx) => {
+          return (
+            <div className="self-stretch flex flex-col items-start justify-start gap-[6px]">
+              <div className="self-stretch rounded-11xl bg-white flex flex-row p-3 items-start justify-start gap-[24px]">
+                <img
+                  className="self-stretch relative max-h-full w-[3px]"
+                  alt=""
+                  src="/curation-title-line.svg"
+                />
+                <div className="w-[319px] flex flex-col items-start justify-start gap-[12px]">
+                  <div className="self-stretch flex flex-row items-center justify-start gap-[6px]">
+                    <div
+                      className={`relative tracking-[0.05em] leading-[125%] font-semibold ${
+                        log.pointType ? 'text-dodgerblue' : 'text-red'
+                      }`}
+                    >
+                      {log.pointType
+                        ? `+${priceToString(log.pointCost)}`
+                        : `-${priceToString(log.pointCost)}`}
+                    </div>
+                    <div className="relative text-base tracking-[0.01em] leading-[125%] font-semibold">
+                      포인트가 {log.pointType ? '지급' : '차감'}되었습니다.
+                    </div>
                   </div>
-                  <div className="relative text-base tracking-[0.01em] leading-[125%] font-semibold">
-                    포인트가 {log.type ? '지급' : '차감'}되었습니다.
+                  <div className="self-stretch relative text-base tracking-[0.01em] leading-[125%] text-slategray">
+                    {log.pointContent}
                   </div>
-                </div>
-                <div className="self-stretch relative text-base tracking-[0.01em] leading-[125%] text-slategray">
-                  {log.message}
                 </div>
               </div>
+              {idx < logs.length - 1 && (
+                <div className="relative bg-whitesmoke-200 w-full h-px" />
+              )}
             </div>
-            {idx < logs.length - 1 && (
-              <div className="relative bg-whitesmoke-200 w-full h-px" />
-            )}
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <div className="text-center">
+          <p>사용 내역이 없습니다.</p>
+        </div>
+      )}
     </div>
   );
 }
