c24a4bf2c1bf7416f420e4f4b5dc3e385016ecb5
yms1789 <yms1789@naver.com>
2023-08-11 16:38:50 +0900

    🚀Feat: 인벤토리 기능 구현
    - 요소 장착 시 버튼 장착 → 해제로 바뀌도록 구현
    - 각 아이템 타입에 따라 전역에 장착한 아이템 데이터 저장
    - 장착할 요소가 없으면 보유한 아이템이 없다는 메시지 출력
    - 장착한 요소에 대한 정보를 서버에 전달

--- frontend/petmily/src/components/Inventory.jsx
+++ frontend/petmily/src/components/Inventory.jsx
@@ -1,52 +1,42 @@
-import { placeholderImage } from 'utils/utils';
-
-const sampleEquipments = {
-  borders: [
-    {
-      id: 0,
-      name: 'border1',
-      element: 'dodgerblue',
-    },
-    {
-      id: 1,
-      name: 'border2',
-      element: 'red',
-    },
-  ],
-  badges: [
-    {
-      id: 0,
-      name: 'badge1',
-      element: placeholderImage(Math.floor(Math.random() * 1001) + 1),
-    },
-    {
-      id: 1,
-      name: 'badge2',
-      element: placeholderImage(Math.floor(Math.random() * 1001) + 1),
-    },
-  ],
-  cover: [
-    {
-      id: 0,
-      name: 'cover1',
-      element: placeholderImage(Math.floor(Math.random() * 1001) + 1),
-    },
-    {
-      id: 1,
-      name: 'cover2',
-      element: placeholderImage(Math.floor(Math.random() * 1001) + 1),
-    },
-  ],
-};
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import userAtom from 'states/users';
+import useFetch from 'utils/fetch';
 
 function Inventory() {
-  const handleElementClick = (buttonType, elementId) => {
-    if (buttonType === '장착') {
-      // 장착한 요소 넘겨주자
-      console.log(buttonType, elementId);
+  const inventoryFetch = useFetch();
+  const [user, setUser] = useRecoilState(userAtom);
+  const { userEmail, userBackground, userBadge, userRing } = user;
+  const [equipments, setEquipments] = useState({});
+  useEffect(() => {
+    async function fetchInventoryData() {
+      try {
+        const inventoryData = await inventoryFetch.get(
+          `item/inventory?userEmail=${userEmail}`,
+        );
+        console.log(inventoryData);
+        setEquipments(inventoryData);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    if (buttonType === '해제') {
-      console.log(buttonType);
+    fetchInventoryData();
+  }, []);
+
+  const handleElementClick = async (elementId, itemType) => {
+    try {
+      const response = await inventoryFetch.put('item/equipment', {
+        userEmail,
+        itemId: elementId,
+        itemType,
+      });
+      console.log(response);
+      if (itemType === 'ring') setUser({ ...user, userRing: response });
+      if (itemType === 'badge') setUser({ ...user, userBadge: response });
+      if (itemType === 'background')
+        setUser({ ...user, userBackground: response });
+    } catch (error) {
+      console.log(error);
     }
   };
   return (
@@ -60,98 +50,150 @@ function Inventory() {
           <div className="relative font-semibold">프로필 링</div>
           <div className="self-stretch relative bg-whitesmoke-200 h-px" />
         </div>
-        {sampleEquipments.borders.map(ele => {
-          return (
-            <div className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[24px]">
-              <div className="self-stretch flex flex-row items-start justify-between">
-                <div className="flex flex-row items-center justify-center gap-[12px]">
-                  <div
-                    className={`relative w-[30px] h-[30px] object-cover border-solid border-2 rounded-[100px] border-${ele.element}`}
-                  />
-                  <div className="relative font-semibold">{ele.name}</div>
-                </div>
-                <div
-                  role="presentation"
-                  className="bg-dodgerblue hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-white rounded-xl cursor-pointer"
-                  onClick={() => {
-                    handleElementClick('장착', ele.id);
-                  }}
-                >
-                  <b className="relative tracking-[0.05em] leading-[19px]">
-                    장착
-                  </b>
+        {equipments.ring ? (
+          equipments.ring.map(ele => {
+            return (
+              <div
+                key={ele.itemName}
+                className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[24px]"
+              >
+                <div className="self-stretch flex flex-row items-start justify-between">
+                  <div className="flex flex-row items-center justify-center gap-[12px]">
+                    <div
+                      className={`relative w-[30px] h-[30px] object-cover border-solid border-2 rounded-[100px] border-${ele.element}`}
+                    />
+                    <div className="relative font-semibold">{ele.itemName}</div>
+                  </div>
+                  {!(userRing.itemId === ele.itemId) ? (
+                    <div
+                      role="presentation"
+                      className="bg-dodgerblue hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-white rounded-xl cursor-pointer"
+                      onClick={() => {
+                        handleElementClick(ele.itemId, 'ring');
+                      }}
+                    >
+                      <b className="relative tracking-[0.05em] leading-[19px]">
+                        장착
+                      </b>
+                    </div>
+                  ) : (
+                    <div
+                      role="presentation"
+                      className="bg-white hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-dodgerblue rounded-xl cursor-pointer border-solid border-2 border-dodgerblue"
+                      onClick={() => {
+                        handleElementClick(null, 'ring');
+                      }}
+                    >
+                      <b className="relative tracking-[0.05em] leading-[19px]">
+                        해제
+                      </b>
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="px-5">보유한 링이 없습니다.</div>
+        )}
       </div>
       <div className="self-stretch rounded-11xl bg-white flex flex-col py-0 px-3 items-start justify-start gap-[12px]">
         <div className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[12px] text-dodgerblue">
           <div className="relative font-semibold">뱃지</div>
           <div className="self-stretch relative bg-whitesmoke-200 h-px" />
         </div>
-        {sampleEquipments.badges.map(ele => {
-          return (
-            <div className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[24px]">
-              <div className="self-stretch flex flex-row items-start justify-between">
-                <div className="flex flex-row items-center justify-center gap-[12px]">
-                  <img
-                    src={ele.element}
-                    alt=""
-                    className="`relative w-[30px] h-[30px] rounded-xl object-cover"
-                  />
-                  <div className="relative font-semibold">{ele.name}</div>
-                </div>
-                <div
-                  role="presentation"
-                  className="box-border hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-[1.1rem] items-center justify-center text-center text-mini text-dodgerblue border-[2px]
-                 border-solid border-dodgerblue rounded-xl cursor-pointer"
-                  onClick={() => {
-                    handleElementClick('해제');
-                  }}
-                >
-                  <b className="relative tracking-[0.05em] leading-[19px]">
-                    해제
-                  </b>
+        {equipments?.badge ? (
+          equipments?.badge.map(ele => {
+            return (
+              <div className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[24px]">
+                <div className="self-stretch flex flex-row items-start justify-between">
+                  <div className="flex flex-row items-center justify-center gap-[12px]">
+                    <div
+                      className={`relative w-[30px] h-[30px] object-cover border-solid border-2 rounded-[100px] border-${ele.element}`}
+                    />
+                    <div className="relative font-semibold">{ele.itemName}</div>
+                  </div>
+                  {!(userBadge.itemId === ele.itemId) ? (
+                    <div
+                      role="presentation"
+                      className="bg-dodgerblue hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-white rounded-xl cursor-pointer"
+                      onClick={() => {
+                        handleElementClick(ele.itemId, 'badge');
+                      }}
+                    >
+                      <b className="relative tracking-[0.05em] leading-[19px]">
+                        장착
+                      </b>
+                    </div>
+                  ) : (
+                    <div
+                      role="presentation"
+                      className="bg-white hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-dodgerblue rounded-xl cursor-pointer border-solid border-2 border-dodgerblue"
+                      onClick={() => {
+                        handleElementClick(null, 'badge');
+                      }}
+                    >
+                      <b className="relative tracking-[0.05em] leading-[19px]">
+                        해제
+                      </b>
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="px-5">보유한 뱃지가 없습니다.</div>
+        )}
       </div>
       <div className="self-stretch rounded-11xl bg-white flex flex-col py-0 px-3 items-start justify-start gap-[12px]">
         <div className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[12px] text-dodgerblue">
           <div className="relative font-semibold">커버 이미지</div>
           <div className="self-stretch relative bg-whitesmoke-200 h-px" />
         </div>
-        {sampleEquipments.cover.map(ele => {
-          return (
-            <div className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[24px]">
-              <div className="self-stretch flex flex-row items-start justify-between">
-                <div className="flex flex-row items-center justify-center gap-[12px]">
-                  <img
-                    src={ele.element}
-                    alt=""
-                    className="`relative w-[30px] h-[30px] rounded-md object-cover"
-                  />
-                  <div className="relative font-semibold">{ele.name}</div>
-                </div>
-                <div
-                  role="presentation"
-                  className="bg-dodgerblue hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-white rounded-xl cursor-pointer"
-                  onClick={() => {
-                    handleElementClick('장착');
-                  }}
-                >
-                  <b className="relative tracking-[0.05em] leading-[19px]">
-                    장착
-                  </b>
+        {equipments.background ? (
+          equipments?.background.map(ele => {
+            return (
+              <div className="self-stretch flex flex-col py-1 px-4 items-start justify-start gap-[24px]">
+                <div className="self-stretch flex flex-row items-start justify-between">
+                  <div className="flex flex-row items-center justify-center gap-[12px]">
+                    <div
+                      className={`relative w-[30px] h-[30px] object-cover border-solid border-2 rounded-[100px] border-${ele.element}`}
+                    />
+                    <div className="relative font-semibold">{ele.itemName}</div>
+                  </div>
+                  {!(userBackground.itemId === ele.itemId) ? (
+                    <div
+                      role="presentation"
+                      className="bg-dodgerblue hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-white rounded-xl cursor-pointer"
+                      onClick={() => {
+                        handleElementClick(ele.itemId, 'background');
+                      }}
+                    >
+                      <b className="relative tracking-[0.05em] leading-[19px]">
+                        장착
+                      </b>
+                    </div>
+                  ) : (
+                    <div
+                      role="presentation"
+                      className="bg-white hover:brightness-90 h-[30px] overflow-hidden flex flex-row py-[15px] px-5 box-border items-center justify-center text-center text-mini text-dodgerblue rounded-xl cursor-pointer border-solid border-2 border-dodgerblue"
+                      onClick={() => {
+                        handleElementClick(null, 'background');
+                      }}
+                    >
+                      <b className="relative tracking-[0.05em] leading-[19px]">
+                        해제
+                      </b>
+                    </div>
+                  )}
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })
+        ) : (
+          <div className="px-5">보유한 커버이미지가 없습니다.</div>
+        )}
       </div>
     </div>
   );
