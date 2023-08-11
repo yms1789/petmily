ca0f236cf7bef0b44e893e86a88b3ae845a335f3
yms1789 <yms1789@naver.com>
2023-08-11 16:41:22 +0900

    🚑Fix: 북마크 수정
    - 정보를 RenderBookmarks 컴포넌트에서 받도록 수정
    - 마이페이지에서 북마크 버튼 클릭시 북마크 해제

--- frontend/petmily/src/components/RenderBookmarks.jsx
+++ frontend/petmily/src/components/RenderBookmarks.jsx
@@ -1,18 +1,17 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
-import { arrayOf, number, shape, string } from 'prop-types';
 import { useRecoilState } from 'recoil';
 import { v4 as uuidv4 } from 'uuid';
 import userAtom from 'states/users';
 import useFetch from 'utils/fetch';
 
-function RenderBookmarks({ renderData }) {
+function RenderBookmarks() {
   const navigation = useNavigate();
   const fetchData = useFetch();
   const [userInfo, setUserInfo] = useRecoilState(userAtom);
+  const [userBookmarks, setUserBookmarks] = useState([]);
   const handleBookmark = useCallback(
     async curationId => {
       if (!userInfo || !Object.keys(userInfo).length) {
@@ -20,41 +19,51 @@ function RenderBookmarks({ renderData }) {
         alert('로그인이 필요합니다.');
       }
       try {
-        const data = await fetchData.post('curation/bookmarks', {
-          userEmail: userInfo.userEmail,
-          cid: curationId,
-        });
-        console.log(data);
-        setUserInfo({ ...userInfo, bookmarks: data });
+        const message = window.confirm('북마크를 취소하시겠습니까?');
+        if (message) {
+          await fetchData.post('curation/bookmarks', {
+            userEmail: userInfo.userEmail,
+            cid: curationId,
+          });
+          setUserBookmarks(prev => prev.filter(ele => ele.cid !== curationId));
+          setUserInfo({
+            ...userInfo,
+            bookmarks: userInfo.bookmarks.filter(ele => ele !== curationId),
+          });
+        }
       } catch (error) {
         console.log(error);
       }
     },
-    [fetchData, navigation, setUserInfo, userInfo],
+    [fetchData, navigation, userInfo],
   );
-  console.log(renderData);
+  useEffect(() => {
+    async function fetchBookmarks() {
+      try {
+        const bookmarks = await fetchData.get(
+          `curation/userbookmarksdetail?userEmail=${userInfo.userEmail}`,
+        );
+        if (bookmarks.length > 0) {
+          console.log('renderBookmarks', bookmarks);
+          setUserBookmarks(bookmarks);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchBookmarks();
+  }, []);
   return (
-    <div className="w-full h-full flex flex-col items-center bg-white gap-5 overflow-y-scroll py-1">
-      {renderData?.map(ele => {
-        return (
-          <div
-            key={uuidv4()}
-            className="relative flex-1 w-[95%] h-full p-5 rounded-11xl bg-slate-100 shadow-md overflow-hidden flex flex-col pt-0 px-0 pb-6 items-center justify-center gap-[16px]"
-          >
-            {/* 유저 북마크 정보랑 비교해서 큐레이션 카드 ID가 북마크 정보 리스트안에 있으면
+    <div className="w-full h-full flex flex-col items-center bg-white gap-5  py-1">
+      {userBookmarks.length > 0 ? (
+        userBookmarks?.map(ele => {
+          return (
+            <div
+              key={uuidv4()}
+              className="relative flex-1 w-[95%] h-full p-5 rounded-11xl bg-slate-100 shadow-md overflow-hidden flex flex-col pt-0 px-0 pb-6 items-center justify-center gap-[16px]"
+            >
+              {/* 유저 북마크 정보랑 비교해서 큐레이션 카드 ID가 북마크 정보 리스트안에 있으면
      TRUE 아니면 FALSE */}
-            {!userInfo?.bookmarks?.includes(ele.cid) ? (
-              <BookmarkBorderIcon
-                className="absolute bottom-2 right-3 cursor-pointer z-10"
-                color="primary"
-                onClick={() => {
-                  handleBookmark(ele.cid);
-                }}
-                fontSize="large"
-              >
-                북마크
-              </BookmarkBorderIcon>
-            ) : (
               <BookmarkIcon
                 className="absolute bottom-2 right-3 cursor-pointer z-10"
                 color="primary"
@@ -62,47 +71,38 @@ function RenderBookmarks({ renderData }) {
                   handleBookmark(ele.cid);
                 }}
               />
-            )}
-            <a
-              href={ele.curl}
-              className="w-fit h-fit no-underline text-black"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                className="relative w-full object-fill"
-                alt=""
-                src={ele.cimage}
-              />
-              <div className="flex flex-col items-start justify-center gap-[16px] p-4 w-fit">
-                <div className="flex flex-row items-center justify-center gap-[12px]">
-                  <div className="relative tracking-[0.01em] leading-[125%] font-medium flex items-start">
-                    {ele.ctitle}
+              <a
+                href={ele.curl}
+                className="w-fit h-fit no-underline text-black"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="relative w-full object-fill"
+                  alt=""
+                  src={ele.cimage}
+                />
+                <div className="flex flex-col items-start justify-center gap-[16px] p-4 w-fit">
+                  <div className="flex flex-row items-center justify-center gap-[12px]">
+                    <div className="relative tracking-[0.01em] leading-[125%] font-medium flex items-start">
+                      {ele.ctitle}
+                    </div>
+                  </div>
+                  <div className="relative text-[0.88rem] tracking-[0.01em] leading-[125%] text-darkgray flex items-center">
+                    {ele.ccontent}
                   </div>
                 </div>
-                <div className="relative text-[0.88rem] tracking-[0.01em] leading-[125%] text-darkgray flex items-center">
-                  {ele.ccontent}
-                </div>
-              </div>
-            </a>
-          </div>
-        );
-      })}
+              </a>
+            </div>
+          );
+        })
+      ) : (
+        <div>
+          <p className=" font-bold">북마크 한 큐레이션이 없습니다.</p>
+        </div>
+      )}
     </div>
   );
 }
-const rederDataType = shape({
-  ctitle: string,
-  cid: number,
-  ccontent: string,
-  cimage: string,
-  cdate: string,
-  curl: string,
-  cpetSpecies: string,
-  ccategory: string,
-});
 
-RenderBookmarks.propTypes = {
-  renderData: arrayOf(rederDataType),
-};
 export default RenderBookmarks;
