4ee476b0bd957a9e5a1d1c4301513f26e673ddbc
yms1789 <yms1789@naver.com>
2023-08-11 17:04:43 +0900

    🚑Fix: 포인트 사용 내역 코드 수정
    - 에러 핸들링을 위해 log 데이터 출력
    - log 리스트 길이가 0보다 작으면 사용내역이 없다고 출력되게끔 수정

--- frontend/petmily/src/components/PointLog.jsx
+++ frontend/petmily/src/components/PointLog.jsx
@@ -2,13 +2,14 @@ import { shape, number, string, instanceOf, arrayOf } from 'prop-types';
 import { priceToString } from 'utils/utils';
 
 function PointLog({ logs }) {
+  console.log('pointLog', logs);
   return (
     <div className="rounded-11xl bg-white min-w-[350px] h-fit overflow-hidden shrink-0 flex flex-col basis-1/4 p-4 box-border items-center justify-start gap-[6px]">
       <div className="flex flex-col items-start justify-center gap-[17px] w-full">
         <div className="relative font-semibold">포인트 내역</div>
         <div className="relative bg-whitesmoke-200 w-full h-px" />
       </div>
-      {logs ? (
+      {logs.length > 0 ? (
         logs.map((log, idx) => {
           return (
             <div className="self-stretch flex flex-col items-start justify-start gap-[6px]">
