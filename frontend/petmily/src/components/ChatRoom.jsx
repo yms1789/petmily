4fcbc5347306d057f1280115f06fa8ce998e96f5
kanghaeven <meerhae@gmail.com>
2023-08-13 07:24:30 +0900

    🚑Fix: confilct 수정

--- frontend/petmily/src/components/ChatRoom.jsx
+++ frontend/petmily/src/components/ChatRoom.jsx
@@ -31,16 +31,7 @@ function ChatRoom() {
   return (
     <div className="mx-4 basis-1/4 flex h-[800px] rounded-xl bg-white min-w-[20%] flex-col p-[1rem] items-start justify-start gap-[0.38rem] font-pretendard">
       <div className="flex w-full flex-col items-start justify-center gap-[1rem] text-[1.25rem]">
-        <div className="flex justify-center items-center">
-          <div className="ml-1 font-semibold">메세지 목록</div>
-          <div
-            role="presentation"
-            className="ml-1 font-semibold"
-            // onClick={createChatRoom}
-          >
-            채팅방 생성
-          </div>
-        </div>
+        <div className="ml-1 font-semibold">메세지 목록</div>
         <div className="bg-slate-200 w-full h-[1.5px]" />
       </div>
       {chatRoom?.map(room => {
