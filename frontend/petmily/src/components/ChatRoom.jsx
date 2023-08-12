d87995db9f2d85ae24078cf9706acb6283a8de89
kanghaeven <meerhae@gmail.com>
2023-08-13 04:34:40 +0900

    🚑Fix: 채팅방 목록 불러오기 백엔드 api 연동

--- frontend/petmily/src/components/ChatRoom.jsx
+++ frontend/petmily/src/components/ChatRoom.jsx
@@ -1,14 +1,51 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import userAtom from 'states/users';
 import { placeholderImage } from 'utils/utils';
 
-const tempMessages = Array.from({ length: 5 }, (_, i) => i);
+// const tempMessages = Array.from({ length: 5 }, (_, i) => i);
 
 function ChatRoom() {
   const navigate = useNavigate();
+  const userLogin = useRecoilValue(userAtom);
+  const [chatRoom, setChatRoom] = useState([]);
 
   const handleOpenChat = () => {
     navigate('/social/chat');
   };
+
+  const readChatRoom = async () => {
+    const sendBE = {
+      sender: userLogin.userNickname,
+      receiver: 'string',
+    };
+    try {
+      const response = await axios.post('chat/history', sendBE);
+      console.log('채팅방 조회', response);
+      setChatRoom(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // const createChatRoom = async recieverNickname => {
+  //   const sendBE = {
+  //     sender: userLogin.userNickname,
+  //     receiver: recieverNickname,
+  //   };
+  //   try {
+  //     const response = await fetchChatRoom.post('chat/start', sendBE);
+  //     console.log('채팅방 생성', response);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  useEffect(() => {
+    readChatRoom();
+  }, []);
 
   return (
     <div className="mx-4 basis-1/4 flex h-[800px] rounded-xl bg-white min-w-[20%] flex-col p-[1rem] items-start justify-start gap-[0.38rem] font-pretendard">
@@ -16,7 +53,7 @@ function ChatRoom() {
         <div className="ml-1 font-semibold">메세지 목록</div>
         <div className="bg-slate-200 w-full h-[1.5px]" />
       </div>
-      {tempMessages.map(ele => {
+      {chatRoom.map(ele => {
         return (
           <div
             role="presentation"
@@ -44,7 +81,7 @@ function ChatRoom() {
                 <b className="">{ele}</b>
               </div>
             </div>
-            {ele < tempMessages.length - 1 ? (
+            {ele < chatRoom.length - 1 ? (
               <div className="bg-slate-200 w-full h-[1px]" />
             ) : null}
           </div>
