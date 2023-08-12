2cb9831af557e59e470c24a35e2fdfb8da878ff8
혜진 <vvloveletter@naver.com>
2023-08-13 00:09:55 +0900

    🔨Refactor : 필요 수정사항 일부 반영

--- back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/user/follow/FollowService.java
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/user/follow/FollowService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface FollowService {
-    String followUser(Long userId, FollowUserDto followUserDto);
+    String followUser(String userEmail, FollowUserDto followUserDto);
 
-    String unfollowUser(Long userId, FollowUserDto followUserDto);
+    String unfollowUser(String userEmail, FollowUserDto followUserDto);
 }
 
