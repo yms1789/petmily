ebeb61899d6fec7a165912e7b7bf7ad37faf30fe
혜진 <vvloveletter@naver.com>
2023-08-13 12:39:29 +0900

    🔨Refactor : 쿼리문 수정

---
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/noti/entity/NotiType.java
@@ -0,0 +1,14 @@
+//package com.pjt.petmily.domain.noti.entity;
+//
+//import lombok.Getter;
+//
+//@Getter
+//public enum NotiType {
+//    LIKE("좋아요"), COMMENT("댓글작성"), FOLLOW("팔로우");
+//
+//    NotiType(String key) {
+//        this.key = key;
+//    }
+//
+//    private String key;
+//}
