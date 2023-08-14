5f381ff43498c7f9074cfbd8e5122fe770162c21
DESKTOP-DOGVPUB\SSAFY <smk921@naver.com>
2023-08-14 13:49:08 +0900

    🚑Fix : 카카오 로그인 코드 수정_ver2.1

---
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/oauth/KakaoDto.java
@@ -0,0 +1,8 @@
+package com.pjt.petmily.domain.oauth;
+
+import lombok.Data;
+
+@Data
+public class KakaoDto {
+    private String code;
+}
--- back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/oauth/OauthController.java
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/oauth/OauthController.java
@@ -25,10 +25,10 @@ public class OauthController {
     @Operation(summary="카카오 로그인", description="카카오 로그인")
     @ResponseBody
     @PostMapping("/login/kakao")
-    public ResponseDto<LoginResponseDto> kakaoCallback(@Parameter(description = "kakao auth code", required = true) @RequestBody String code){
+    public ResponseDto<LoginResponseDto> kakaoCallback(@Parameter(description = "kakao auth code", required = true) @RequestBody KakaoDto kakaoDto){
 
         // 코드를 이용해서 카카오서버로부터 accessToken 발급
-        String accessToken = oAuthService.getKakaoAccessToken(code);
+        String accessToken = oAuthService.getKakaoAccessToken(kakaoDto.getCode());
 
         // accessToken으로 유저정보를 받아옴
 //        HashMap<String, Object> userInfo = oAuthService.getUserInfo(accessToken);
