68194ee6f9b9588e535aa54430f9c29f75d1792d
DESKTOP-DOGVPUB\SSAFY <smk921@naver.com>
2023-08-15 01:01:57 +0900

    🚑Fix: 산책수정_ver1.4

--- back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/pet/dto/WalkDto.java
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/pet/dto/WalkDto.java
@@ -2,9 +2,12 @@ package com.pjt.petmily.domain.pet.dto;
 
 import com.pjt.petmily.domain.pet.Walk;
 import com.pjt.petmily.domain.pet.entity.Pet;
+import lombok.Data;
 
 import java.util.List;
 
+
+@Data
 public class WalkDto {
     private Pet pet;
     private List<Walk> walks;
