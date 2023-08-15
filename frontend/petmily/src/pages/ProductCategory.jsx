e2debaecac9b7fa5e89d1a10305eb5d7cb0b5dd6
여민수 <yms1789@naver.com>
2023-08-16 00:54:50 +0900

    💄Design: 인기용품 최저가 탭 CSS 수정
    - 헤더 고정에 따른 요소 위치 수정

--- frontend/petmily/src/pages/ProductCategory.jsx
+++ frontend/petmily/src/pages/ProductCategory.jsx
@@ -12,7 +12,7 @@ function ProductCategory() {
   console.log('petCurations', product[productCategory], productCategory);
 
   return (
-    <div className="min-w-[1340px] max-w-full flex flex-1 flex-col items-center justify-center text-left text-[1.13rem] text-darkgray font-pretendard">
+    <div className="absolute top-20 min-w-[1340px] max-w-full flex flex-1 flex-col items-center justify-center text-left text-[1.13rem] text-darkgray font-pretendard">
       <div className="min-w-[1340px] max-w-full relative text-[1.75rem] text-gray">
         <div className=" flex p-[40px] flex-col items-start justify-start text-[1.5rem] text-white">
           <img
