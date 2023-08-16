a6d78075ff27a8e6e6a38a62fb487b52bdb9acf3
혜진 <vvloveletter@naver.com>
2023-08-16 09:25:26 +0900

    🚑Fix : FCM 임시 비활성화

--- back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/global/FCM/FirebaseConfig.java
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/global/FCM/FirebaseConfig.java
@@ -1,23 +1,24 @@
-package com.pjt.petmily.global.FCM;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.io.FileInputStream;
-import java.io.IOException;
-
-@Configuration
-public class FirebaseConfig {
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccountFile = new FileInputStream("src/main/resources/google-services.json");
-        FirebaseOptions options = FirebaseOptions
-                .builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile))
-                .build();
-        return FirebaseApp.initializeApp(options);
-    }
-}
+//package com.pjt.petmily.global.FCM;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//@Configuration
+//public class FirebaseConfig {
+//    @Bean
+//    public FirebaseApp firebaseApp() throws IOException {
+//        FileInputStream serviceAccountFile = new FileInputStream("resources/google-services.json");
+//        FirebaseOptions options = FirebaseOptions
+//                .builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile))
+//                .build();
+//        return FirebaseApp.initializeApp(options);
+//    }
+//}
