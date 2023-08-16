e75a43961802cf8443c4e460dec1c9950cab9a5f
혜진 <vvloveletter@naver.com>
2023-08-16 15:03:53 +0900

    🔨Refactor : FCM 코드 2차 추가

--- back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/chat/controller/StompChatController.java
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/chat/controller/StompChatController.java
@@ -1,19 +1,12 @@
 package com.pjt.petmily.domain.chat.controller;
 
 import com.pjt.petmily.domain.chat.dto.ChatMessageDTO;
-import com.pjt.petmily.domain.chat.entity.ChatRoom;
-import com.pjt.petmily.domain.chat.repository.ChatRoomJpaRepository;
-import com.pjt.petmily.domain.chat.repository.ChatRoomRepository;
-import com.pjt.petmily.domain.user.User;
-import com.pjt.petmily.global.FCM.FirebaseCloudMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.io.IOException;
 
 @Log4j2
 @Controller
@@ -23,33 +16,12 @@ public class StompChatController {
     private final SimpMessagingTemplate template; //특정 Broker로 메세지를 전달
     @Autowired
     private com.pjt.petmily.service.ChatMessageService chatMessageService;
-    @Autowired
-    private ChatRoomJpaRepository chatRoomRepository;
-    private FirebaseCloudMessageService firebaseCloudMessageService;
 
 
     @MessageMapping(value = "/message")
-    public void message(ChatMessageDTO message) throws IOException {
+    public void message(ChatMessageDTO message){
         log.info("메세지 전송");
         chatMessageService.saveMessage(message);
         template.convertAndSend("/sub/room/" + message.getRoomId(), message);
-
-        // 1. 대화방 찾기
-        ChatRoom chatRoom = chatRoomRepository.findByRoomId(message.getRoomId())
-                .orElseThrow(() -> new RuntimeException("대화방을 찾을 수 없음: " + message.getRoomId()));
-
-        // 2. 메시지를 보낸 사람을 제외한 나머지 참여자들의 FCM 토큰을 가져와서 알림 전송
-        for (User participant : chatRoom.getParticipants()) {
-            if (!participant.getUserEmail().equals(message.getWriter())) {
-                String fcmToken = participant.getFcmToken();
-
-                if (fcmToken != null && !fcmToken.isEmpty()) {
-                    String title = "새로운 메시지 알림";
-                    String body = message.getWriter() + "님으로부터 새로운 메시지가 도착하였습니다.";
-
-                    firebaseCloudMessageService.sendMessageTo(fcmToken, title, body, message.getRoomId());
-                }
-            }
-        }
     }
 }
--- back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/chat/service/ChatMessageService.java
+++ back_end/petmily_pjt_temp/src/main/java/com/pjt/petmily/domain/chat/service/ChatMessageService.java
@@ -5,20 +5,28 @@ import com.pjt.petmily.domain.chat.dto.ChatMessageDTO;
 import com.pjt.petmily.domain.chat.entity.ChatRoom;
 import com.pjt.petmily.domain.chat.repository.ChatMessageJpaRepository;
 import com.pjt.petmily.domain.chat.repository.ChatRoomJpaRepository;
+import com.pjt.petmily.domain.user.User;
+import com.pjt.petmily.global.FCM.FirebaseCloudMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
 
 @Service
 public class ChatMessageService {
 
     @Autowired
     private ChatMessageJpaRepository chatMessageJpaRepository;
+
     @Autowired
     private ChatRoomJpaRepository chatRoomJpaRepository;
 
+    @Autowired
+    private FirebaseCloudMessageService firebaseCloudMessageService;
+
     @Transactional
-    public ChatMessage saveMessage(ChatMessageDTO messageDTO) {
+    public ChatMessage saveMessage(ChatMessageDTO messageDTO) throws IOException {
         // 채팅방 검색
         ChatRoom chatRoom = chatRoomJpaRepository.findByRoomId(messageDTO.getRoomId())
                 .orElseThrow(() -> new RuntimeException("채팅방이 존재 하지 않음 " + messageDTO.getRoomId()));
@@ -34,12 +42,22 @@ public class ChatMessageService {
 
         // chatRoom의 최신 메시지로 설정
         chatRoom.setLatestMessage(savedMessage);
-
-
-
         // chatRoom 업데이트
         chatRoomJpaRepository.save(chatRoom);
 
+        // 2. 메시지를 보낸 사람을 제외한 나머지 참여자들의 FCM 토큰을 가져와서 알림 전송
+        for (User participant : chatRoom.getParticipants()) {
+            if (!participant.getUserEmail().equals(messageDTO.getWriter())) {
+                String fcmToken = participant.getFcmToken();
+
+                if (fcmToken != null && !fcmToken.isEmpty()) {
+                    String title = "새로운 메시지 알림";
+                    String body = messageDTO.getWriter() + "님으로부터 새로운 메시지가 도착하였습니다.";
+
+                    firebaseCloudMessageService.sendMessageTo(fcmToken, title, body, messageDTO.getRoomId());
+                }
+            }
+        }
         // 저장된 메시지 반환
         return savedMessage;
     }
