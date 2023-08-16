ed58cfed71e6ad9c3a36777e3a5c1bab3ad07305
yms1789 <yms1789@naver.com>
2023-08-16 15:23:07 +0900

    💄Design: 이미지 삭제

--- frontend/petmily/src/pages/ProductCategory.jsx
+++ frontend/petmily/src/pages/ProductCategory.jsx
@@ -2,25 +2,17 @@ import { useLocation } from 'react-router';
 import { useRecoilValue } from 'recoil';
 
 import { RenderProducts } from 'components';
-import { placeholderImage } from 'utils/utils';
 import productAtom from 'states/products';
 
 function ProductCategory() {
   const location = useLocation();
   const { productCategory } = location.state;
   const product = useRecoilValue(productAtom);
-  console.log('petCurations', product[productCategory], productCategory);
 
   return (
     <div className="absolute top-20 min-w-[1340px] max-w-full flex flex-1 flex-col items-center justify-center text-left text-[1.13rem] text-darkgray font-pretendard">
       <div className="min-w-[1340px] max-w-full relative text-[1.75rem] text-gray">
         <div className=" flex p-[40px] flex-col items-start justify-start text-[1.5rem] text-white">
-          <img
-            className="relative w-full h-[200px] rounded-[20px]"
-            alt=""
-            src={placeholderImage(Math.floor(Math.random() * 1001) + 1)}
-          />
-          <div className="h-20" />
           <div className="h-10" />
 
           <RenderProducts
