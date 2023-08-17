3ec1207657b0b91e64b8ed9614b59d533e35e12e
kanghaeven <meerhae@gmail.com>
2023-08-17 17:22:04 +0900

    🚑Fix: 팔로우 새로고침 목록 유지

--- frontend/petmily/src/states/follow.js
+++ frontend/petmily/src/states/follow.js
@@ -1,8 +1,11 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
 
+const { persistAtom } = recoilPersist();
 const followAtom = atom({
   key: 'followatom',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export default followAtom;
